60c8ccfac91950af6fadd69430b845fd78b1d7c3
Fionarin <vonbouvierazula@gmail.com>
2024-09-11 13:01:53 +0200

    Bugfix: url field

--- locallib.php
+++ locallib.php
@@ -109,7 +109,7 @@ class ableplayer {
             foreach ($formdata->media as $key => $value) {
                 $media = new stdClass();
                 $media->ableplayerid = $returnid;
-                $media->url = $formdata->url;
+                $media->url = $formdata->url[$key];
                 $mediaid = $DB->insert_record("ableplayer_media", $media, true);
                 // Storage of files from the filemanager (captions).
                 $draftitemid = $value;
@@ -224,7 +224,7 @@ class ableplayer {
                 if (isset($formdata->mediaid[$key]) && !empty($formdata->mediaid[$key])) {//existing choice record
                     $media = new stdClass();
                     $media->ableplayerid = $formdata->instance;
-                    $media->url = $formdata->url;
+                    $media->url = $formdata->url[$key];
                     $media->id = $formdata->mediaid[$key];
                     $draftitemid = $value;
                     if ($draftitemid) {
@@ -246,7 +246,7 @@ class ableplayer {
                 } else {
                     $media = new stdClass();
                     $media->ableplayerid = $formdata->instance;
-                    $media->url = $formdata->url;
+                    $media->url = $formdata->url[$key];
                     $mediaid = $DB->insert_record("ableplayer_media", $media, true);
                     // Storage of files from the filemanager (media).
                     $draftitemid = $value;
