fd83ed7f1ff944729aba39ffe2d5a383fd940ac3
Fionarin <vonbouvierazula@gmail.com>
2024-09-27 11:41:47 +0200

    lang strings update

---
+++ lang/de/ableplayer.php
@@ -0,0 +1,66 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * English strings for ableplayer
+ *
+ * You can have a rather longer description of the file as well,
+ * if you like, and it can span multiple lines.
+ * @copyright  2024 Wunderbyte GmbH
+ * @package    mod_ableplayer
+ * @author     T6nis Tartes <tonis.tartes@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['ableplayer:addinstance'] = 'Eine neue AblePlayer-Instanz hinzufügen';
+$string['ableplayer:addinstance'] = 'AblePlayer hinzufügen';
+$string['ableplayercaptions'] = 'Untertitel';
+$string['ableplayercaptions_help'] = 'Hilfe zu Untertiteln';
+$string['ableplayerdescriptionfile'] = 'Beschreibung Datei';
+$string['ableplayergeneral'] = 'Allgemeine Einstellungen';
+$string['ableplayername'] = 'AblePlayer Name';
+$string['ableplayername_help'] = 'Dies ist der Inhalt des Tooltip-Hilfetextes, der dem Feld "AblePlayer Name" zugeordnet ist.';
+$string['ableplayermedia'] = 'Mediendatei';
+$string['ableplayermediaurl'] = 'Medien-URL';
+$string['bgcolor1'] = 'Dunkel';
+$string['bgcolor1_desc'] = 'Hintergrundfarbe';
+$string['bgcolor2'] = 'Standard';
+$string['bgcolor2_desc'] = 'Hintergrundfarbe';
+$string['bgcolor3'] = 'Kick';
+$string['bgcolor3_desc'] = 'Hintergrundfarbe';
+$string['captions'] = 'Untertitel';
+$string['filearea_captions'] = 'Untertitel-Dateien';
+$string['filearea_descs'] = 'Beschreibung Dateien';
+$string['filearea_medias'] = 'Mediendateien';
+$string['filearea_posters'] = 'Poster Bild';
+$string['kind'] = 'Art';
+$string['label'] = 'Label';
+$string['lang'] = 'Player-Sprache';
+$string['lyricsmode'] = 'Liedtextmodus';
+$string['mode'] = 'Modus';
+$string['modulename'] = 'AblePlayer';
+$string['modulename_help'] = 'Das AblePlayer-Modul ermöglicht die Wiedergabe von .mp4, .webm, .ogv, .mp3, .ogg, .oga, .wav Dateien';
+$string['modulenameplural'] = 'AblePlayers';
+$string['playlist'] = 'Playlist';
+$string['playsinline'] = 'Wird inline abgespielt';
+$string['pluginadministration'] = 'AblePlayer Verwaltung';
+$string['pluginname'] = 'AblePlayer';
+$string['poster'] = 'Poster Bild';
+$string['poster_help'] = 'Poster Bild (.jpg, .jpeg, .png nur)';
+$string['settings'] = 'AblePlayer';
+$string['srclang'] = 'Sprache';
